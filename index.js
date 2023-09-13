4844183170af00e6d2a403d8a77d6e18e22fd157
evillarreal18 <ericvillarreal81997@gmail.com>
2023-09-12 19:22:29 -0500

    wrote the code to write the README file and initialize the app

--- index.js
+++ index.js
@@ -51,10 +51,17 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+        return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        console.log("Creating Professional README.md File...");
+        writeToFile("./dist/README.md", generateMarkdown({ ...responses }));
+      });    
+}
 
 // Function call to initialize app
 init();
